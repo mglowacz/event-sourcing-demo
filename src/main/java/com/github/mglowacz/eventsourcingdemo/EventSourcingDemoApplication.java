f54e203d34cf33e620c7a69ea270e7b5d7953954
mglowacz <mglowacz@omnis.pl>
2019-03-19 15:18:33 +0100

    Initial commit

---
+++ src/main/java/com/github/mglowacz/eventsourcingdemo/EventSourcingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.mglowacz.eventsourcingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSourcingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventSourcingDemoApplication.class, args);
+	}
+
+}
